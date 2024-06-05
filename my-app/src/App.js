816c21c8c402bd5c63018ea0d873693355fe8d8a
darpan saini <darpansaini1997@gmail.com>
2024-06-05 14:53:21 +0530

    work in prog

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import { ChatEngine } from "react-chat-engine"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <ChatEngine
+       height="100vh"
+       projectId="20eefa03-24fe-4f7c-8920-412a6e59f2e6"
+       userName="Darpan"
+       userSecret="Darpan@1234"
+      />
     </div>
   );
 }
